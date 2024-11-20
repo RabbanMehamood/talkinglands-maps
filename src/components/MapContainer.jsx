44329fb2bfe1c844e26bd1404a3bb1c720c2be13
RabbanMehamood <rabbansk1729@gmail.com>
2024-11-21 00:12:55 +0530

    MapContainer Leaflet Import

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { MapContainer, Tilelayer, Marker, Popup, Polygon } from "react-leaflet";
-import { fetchSpatialData
-} from '../api/sampleSpatialData';
+import { fetchSpatialData } from "../api/sampleSpatialData";
 import "leaflet/dist/leaflet.css";
 import "./../styles/MapContainer.css";
 
-import React from 'react'
+import React from "react";
 
-function MapContainer({onFeaturnSelect}) {
-  return (
-    <div>
-      
-    </div>
-  )
+function MapContainer({ onFeatureSelect }) {
+    return (
+      <MapContainer
+        center={[37.7749, -122.4194]}
+        zoom={10}
+        className="map-container"
+      ></MapContainer>
+    );
 }
 
-export default MapContainer
+export default MapContainer;
